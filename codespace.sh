9c48a16f2fecbcec122aa34ac8f65277d2d388fb
Felipe Rodrigues <81721938+Comunalti@users.noreply.github.com>
2024-11-24 22:44:10 +0000

    feat(flow):novos nodos para moedas diferentes adicionados

--- codespace.sh
+++ codespace.sh
@@ -2,4 +2,5 @@ sudo apt update && sudo apt install -y curl
 curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 sudo apt install -y nodejs
 sudo npm install -g --unsafe-perm node-red
+npm install --prefix ./nodered/
 bash ./start.sh
